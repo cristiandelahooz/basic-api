24ce4ebd8559f3564de3a7ec0dac6657637f2dc7
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-02 18:29:11 -0400

    docs: remove App.java docs

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,45 +4,41 @@ import io.javalin.Javalin;
 
 public class App {
 
-    public static void main(String[] args) {
-        /**
-         * This is a simple example of how to create a simple web server using Javalin.
-         * We create a simple HTML page and serve it using Javalin.
-         */
-        String html = """
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>Hello World</title>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                                margin: 0;
-                                padding: 0;
-                                background-color: #f0f0f0;
-                            }
-                            h1 {
-                                color: #333;
-                                text-align: center;
-                                margin-top: 50px;
-                            }
-                            p {
-                                color: #666;
-                                text-align: center;
-                                margin-top: 20px;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <h1>Hello World</h1>
-                        <p>Javalin is a simple web framework for Java and Kotlin.</p>
-                        <p>It is lightweight and has a simple and expressive API.</p>
-                    </body>
-                </html>
-                        """;
-        Javalin app = Javalin.create().start(7080);
+  public static void main(String[] args) {
+    String html =
+        """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 0;
+                        padding: 0;
+                        background-color: #f0f0f0;
+                    }
+                    h1 {
+                        color: #333;
+                        text-align: center;
+                        margin-top: 50px;
+                    }
+                    p {
+                        color: #666;
+                        text-align: center;
+                        margin-top: 20px;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Hello World</h1>
+                <p>Javalin is a simple web framework for Java and Kotlin.</p>
+                <p>It is lightweight and has a simple and expressive API.</p>
+            </body>
+        </html>
+        """;
+    Javalin app = Javalin.create().start(7080);
 
-        app.get("/", ctx -> ctx.html(html));
-
-    }
+    app.get("/", ctx -> ctx.html(html));
+  }
 }
